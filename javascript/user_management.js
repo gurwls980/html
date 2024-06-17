afd037d3ecbd3483a610d2f3e6e88ca626f62c4f
gurwls980 <dingdong0907@naver.com>
2024-06-17 17:45:49 +0900

    0617수업

--- javascript/user_management.js
+++ javascript/user_management.js
@@ -1,8 +1,12 @@
-const names = [ "김준일", "김준이", "김준삼" ];
-console.log(names.join(""));
+// inputMode = 1 > 추가
+// inputMode = 2 > 수정
+let inputMode = 1;
 
 let userList = [];
+loadUserList();
+
 let emptyUser = {
+    id: 0,
     name: "",
     username: "",
     password: ""
@@ -14,13 +18,16 @@ let user = {
 
 function renderTable() {
     const userTableBody = document.querySelector(".user-table-body");
-    userTableBody.innerHTML = userList.map(({name ,username, password}, index) => {
+    userTableBody.innerHTML = userList.map(({id, name, username, password}, index) => {
         return `
             <tr>
+                <td><input type="checkbox" onchange="handleUserCheck(event)"></td>
                 <td>${index + 1}</td>
+                <td>${id}</td>
                 <td>${name}</td>
                 <td>${username}</td>
                 <td>${password}</td>
+                <th><button onclick="deleteUser(event)" value="${id}">삭제</button></th>
             </tr>
         `;
     }).join("");
@@ -36,7 +43,7 @@ function handleUserInputKeyDown(e) {
         const nameInput = document.querySelector(".name-input");
         const passwordInput = document.querySelector(".password-input");
         const usernameInput = document.querySelector(".username-input");
-
+        
         if(e.target.name === "name") {
             usernameInput.focus();
         }
@@ -44,8 +51,9 @@ function handleUserInputKeyDown(e) {
             passwordInput.focus();
         }
         if(e.target.name === "password") {
-            userList = [ ...userList, { ...user } ];
+            userList = [ ...userList, { ...user, id: getNewId() } ];
 
+            savaUserList()
             renderTable()
 
             nameInput.value = emptyUser.name
@@ -53,6 +61,33 @@ function handleUserInputKeyDown(e) {
             passwordInput.value = emptyUser.password
 
             nameInput.focus();
+            
         }
     }
+}
+
+function savaUserList() {
+    localStorage.setItem("userList", JSON.stringify(userList));
+}
+
+function loadUserList() {
+    const lsUserList = localStorage.getItem("userList");
+    userList = !lsUserList ? [] : JSON.parse(lsUserList);
+    renderTable();
+}
+
+function deleteUser(e) {
+    userList = userList.filter(({id}) => id !== parseInt(e.target.value));
+    savaUserList();
+    renderTable();
+}
+
+function getNewId() {
+    const userIds = userList.map(user => user.id).sort();
+    const maxUserId = userIds.length === 0 ? 20040000 : Math.max.apply(null, userIds);
+    return maxUserId + 1;
+}
+
+function handleUserCheck(e) {
+    
 }
